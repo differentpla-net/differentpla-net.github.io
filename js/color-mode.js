6761b1e171bd2cdff16117ff2051741567a4bbed
Roger Lipscombe <roger@differentpla.net>
2025-07-06 17:42:23 +0100

    Fix code sections in dark mode

--- js/color-mode.js
+++ js/color-mode.js
@@ -19,11 +19,25 @@
     return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
   }
 
+  const _setTheme = theme => {
+    document.documentElement.setAttribute('data-bs-theme', theme)
+
+    document.querySelectorAll('link[rel=stylesheet].alternate').forEach(
+        x => {
+            if (x.id == theme) {
+                x.media = ''
+            } else {
+                x.media = 'none'
+            }
+        }
+    )
+  }
+
   const setTheme = theme => {
     if (theme === 'auto') {
-      document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
+      _setTheme((window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
     } else {
-      document.documentElement.setAttribute('data-bs-theme', theme)
+      _setTheme(theme)
     }
   }
 
